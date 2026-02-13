68218cc657c58954a5536cae35082f082d142fcb
Lucas Favaro <paradigma0621@gmail.com>
2026-02-13 20:48:38 -0300

    Publishing the one topic, 2 partitions and 1 replica

---
+++ str-producer/src/test/java/com/paradigma0621/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigma0621.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
